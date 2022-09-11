b5ece6558c538ddecbb6169d599af48b910905f9
Seshasai Pamulapati <89412075+SESHASAIP@users.noreply.github.com>
2022-09-11 07:59:44 -0500

    Add files via upload

---
+++ SampleWebAppDemoo/src/main/java/com/example/demo/SampleWebAppDemooApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleWebAppDemooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleWebAppDemooApplication.class, args);
+	}
+
+}
